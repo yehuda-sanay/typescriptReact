6d0ff6a9f071ae3b20d93b075ca8589519c33e00
yehuda-sanay <yehudasanay1@gmail.com>
2022-09-19 18:01:36 +0300

    19.09.22

--- src/services/devices.service.ts
+++ src/services/devices.service.ts
@@ -1,3 +1,3 @@
-const DAPI="https://my-json-server.typicode.com/Jeck99/fake-server/devices"
+// const DAPI="https://my-json-server.typicode.com/Jeck99/fake-server/devices"
 
-export default getDevices= async():Promise<any> = 
+// export default getDevices= async():Promise<any> = 
