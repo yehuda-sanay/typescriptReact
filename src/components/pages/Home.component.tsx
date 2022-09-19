47e03410c8b471d46d469d854091a0dcfdf4628a
yehuda-sanay <yehudasanay1@gmail.com>
2022-09-19 12:30:23 +0300

    19.09.22

--- src/components/pages/Home.component.tsx
+++ src/components/pages/Home.component.tsx
@@ -1,19 +1,41 @@
 import {getUsers} from '../../services/User.service'
 import {IUser} from '../../models/Usermodel'
+import { useEffect, useState } from 'react';
+import { getValue } from '@testing-library/user-event/dist/utils';
+
+let tempUsers: IUser[];
 function Home(){
+    const [usersArray, setUsersArray] = useState(tempUsers)
+    useEffect(()=>{
+        getUsers().then((users:IUser[])=>{
+            setUsersArray(users)
+        })
+    },[])
 
-const getUsersArray=async()=>{
 
-    const userArray=await getUsers().then((result:IUser[])=>result)
-    userArray.map(user=>user.age)
-    
-}
-getUsersArray()
+
     return(
         <div>
             <h1>Home</h1>
-              
+              <table>
+                <thead>
+                    {usersArray?(
+                    Object.keys(usersArray[0]).map((key)=> <th>{key}</th> )
+                    ): (<th></th>)}
+                </thead>
+                <tbody>
+                    {usersArray?.map((user) => (
+                    <tr>
+                        {Object.values(user).map((value) => (
+                            typeof(value) =="string"?
+                            <td>{value}</td>:
+                            <td>{value.first+""+ value.last}</td>
+                   ))}
+                   </tr>
+                    ))}
+                </tbody>
+              </table>
         </div>
-    )
+    );
 }
 export default Home;
